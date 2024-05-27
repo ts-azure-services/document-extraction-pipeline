da861a34b9b61af02ddc107553b3bc195890fd98
Santosh Thomas <thomassantosh@gmail.com>
2024-05-26 22:03:58 -0700

    Changes as of 2024-05-26_22:03:58

--- Makefile
+++ Makefile
@@ -33,7 +33,19 @@ create-datastores:
 		--datastore_name "textfiles" \
 		--datastore_desc "Final text files"
 
+# For a sample PDF, refer the azure-ai-studio documentation; this should be a +700 page pdf
+## If running in the cloud shell, upload the azure-ai-studio pdf
+# Then, use the script below to generate several PDFs out of this base PDF and upload to blob
+input_file="./azure-ai-studio.pdf"
+number_of_pdfs=6
+create-pdfs:
+	rm -rf ./data
+	mkdir -p ./data
+	.venv/bin/python ./setup/create_pdfs.py --input_file=$(input_file) --number_of_pdfs=$(number_of_pdfs)
+
+
 # Upload files to blob container = pdf-files
+# Note that if there is a folder structure inherent in the data folder ahead of time, it will just upload discrete files
 upload-files:
 	.venv/bin/python ./setup/upload_data.py
 
---
+++ setup/create_pdfs.py
@@ -0,0 +1,36 @@
+import argparse
+import random
+import uuid
+import logging
+from pypdf import PdfReader, PdfWriter
+
+def create_random_pdf(input_pdf, num_of_pdfs):
+    """Create new PDFs based upon page count and number"""
+    reader = PdfReader(open(input_pdf, 'rb'))
+
+    for _ in range(num_of_pdfs):
+        writer = PdfWriter()
+        filename = f"{uuid.uuid4()}.pdf"
+        num_pages = random.randint(100,250)
+        for _ in range(num_pages):
+            page_number = random.randint(0, len(reader.pages) - 1)
+            page = reader.pages[page_number]
+            writer.add_page(page)
+
+        with open('./data/' + filename, 'wb') as output:
+            writer.write(output)
+        logging.info(f"Wrote out {filename} with {num_pages} pages.")
+
+def main(input, number_of_pdfs):
+    create_random_pdf(input, number_of_pdfs)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Create PDFs')
+    parser.add_argument('--input_file', type=str, required=True, help='The input file')
+    parser.add_argument('--number_of_pdfs', type=str, required=True, help='The number of files to create')
+    args = parser.parse_args()
+
+    main(args.input_file, int(args.number_of_pdfs))
